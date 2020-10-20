5fbe79374a02e5b5ad3f7d6da282e8868be582e0
magical-tutorial-4913 <evanfujita@gmail.com>
2020-10-20 01:43:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,9 +19,9 @@ def valid_move?(board, index)
 end
 
 def position_taken?(board, index)
-  if (board[index] == " " || board[index] == "" || board[index] == "  " || board[index] == nil)
+  if (board[index] == " " || board[index] == "" || board[index] == "  ")
     true
-  elsif (board[index] == "X" || board[index] == "O" )
+  elsif (board[index] == "X" || board[index] == "O" || board[index] == nil)
     false
 end
 end
